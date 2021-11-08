a3b29a59bef3349cb92268a35af094dd0e4f492a
IhorShmidt <ihor.shmidt@techmagic.co>
2021-11-08 16:36:31 +0200

    temp

--- routes/article.route.js
+++ routes/article.route.js
@@ -7,22 +7,8 @@ router.post('/', articleRoutes.createArticle)
     ;
 
 
-async function userMiddleware (req, res, next) {
-    try {
-        const token = req.headers['Authorization'];
-
-        const userId = util.getUserIdByToken(token);
-
-        const user = util.extractUser(userId);
-        req.user;
-        next();
-    } catch (error) {
-        throw error;
-    }
-}
-
 router
-    .put('/:articleId', userMiddleware, articleRoutes.updateArticle)
+    .put('/:articleId', articleRoutes.updateArticle)
     .delete('/:articleId', articleRoutes.deleteArticle)
 
 module.exports = router;
