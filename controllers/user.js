e624ed60504214c8049292537e2646bf1260921a
IhorShmidt <ihor.shmidt@techmagic.co>
2021-11-08 16:03:46 +0200

    prepart

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,26 @@
+'use strict';
+
+const _ = require('lodash');
+const User = require('../models/user');
+
 module.exports = {createUser};
 
-function createUser(req, res, next) {
-  return res.json({work: true});
+async function createUser(req, res, next) {
+    try {
+        const fields = [
+            'firstName',
+            'lastName',
+            'email',
+            'age',
+        ]
+        const payload = _.pick(req.body, fields);
+        const user = new User(payload);
+        // ----
+        const newUser = await user.save();
+
+        return res.status(200).json(newUser);
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
 }
--- models/user.js
+++ models/user.js
@@ -1,6 +1,43 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({});
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        min: 2, max: 80
+    },
+    lastName: {
+        type: String,
+        required: true,
+        min: 2, max: 80
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 2, max: 80
+    },
+    age: {
+        type: Number,
+        min: 0, max: 130
+    },
+    updateCounter: {
+        type: Number,
+        min: 0,
+        default: 0
+    }
+}, {
+    timestamps: true
+});
+
+UserSchema.methods.fullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+}
+
+UserSchema.pre('save', (next) => {
+    this.updateCounter++;
+    // this.save();
+    next()
+})
 
 module.exports = mongoose.model('User', UserSchema);
